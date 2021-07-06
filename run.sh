617a74f12fb3e6702dc4bea60615b8ea6a9c86f7
4kangjc <73515066+4kangjc@users.noreply.github.com>
2021-07-06 23:37:32 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,12 +7,13 @@
  #sudo rm gcc g++
  #sudo ln -s gcc-11 gcc
  #sudo ln -s g++-11 g++
- sudo apt install flex texinfo
- cd ~ && mkdir DownLoad
- cd DownLoad
- wget http://cgdb.me/files/cgdb-0.7.0.tar.gz
- tar -zvxf cgdb-0.7.0.tar.gz
- cd cgdb-0.7.0
- ./configure –prefix=/usr/local
- sudo make && make install
+ #sudo apt install flex texinfo
+ #cd ~ && mkdir DownLoad
+ #cd DownLoad
+ #wget http://cgdb.me/files/cgdb-0.7.0.tar.gz
+ #tar -zvxf cgdb-0.7.0.tar.gz
+ #cd cgdb-0.7.0
+ #./configure –prefix=/usr/local
+ #sudo make && make install
+ sudo apt install cgdb
  echo 'finish!'
