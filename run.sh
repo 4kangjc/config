6858d93ff5f98fa0dd4a1ad49aacaf2f99cb6eb4
4kangjc <73515066+4kangjc@users.noreply.github.com>
2021-07-06 23:33:05 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,18 @@
+ #! /bin/bash
+ sudo apt install vim g++ gcc golang gdb tree curl net-tools libboost-all-dev openssh-server
+ sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+ sudo apt-get update
+ sudo apt install gcc-11 g++-11
+ cd /bin/usr
+ #sudo rm gcc g++
+ #sudo ln -s gcc-11 gcc
+ #sudo ln -s g++-11 g++
+ sudo apt install flex texinfo
+ cd ~ && mkdir DownLoad
+ cd DownLoad
+ wget http://cgdb.me/files/cgdb-0.7.0.tar.gz
+ tar -zvxf cgdb-0.7.0.tar.gz
+ cd cgdb-0.7.0
+ ./configure –prefix=/usr/local
+ sudo make && make install
+ echo 'finish!'
