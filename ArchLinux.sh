8130c07deeaaf008a0c4a74309b2f63343495284
“â4kangjc <1158035520@qq.com>
2021-08-02 22:10:38 +0800

    update

--- ArchLinux.sh
+++ ArchLinux.sh
@@ -2,4 +2,7 @@
 sudo pacman -S wget git vim gcc go gdb cgdb tree curl net-tools tcpdump boost openssh man man-pages neofetch
 locale-gen "en_US.UTF-8"
 locale-gen "zh_CN.UTF-8"
+sudo pacman -S zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+chsh -s /bin/zsh
 echo 'finish'
