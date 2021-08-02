16b6234bffa6514e65ac5b29660a165a47f7babe
“â4kangjc <1158035520@qq.com>
2021-08-02 16:48:33 +0800

    update

--- ArchLinux.sh
+++ ArchLinux.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
-sudo pacman -S wget git vim gcc go gdb cgdb tree curl net-tools tcpdump boost openssh
+sudo pacman -S wget git vim gcc go gdb cgdb tree curl net-tools tcpdump boost openssh man man-pages neofetch
+locale-gen "en_US.UTF-8"
+locale-gen "zh_CN.UTF-8"
 echo 'finish'
