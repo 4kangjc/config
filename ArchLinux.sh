9145c688e4eca25d94cda15591920c089612c540
“â4kangjc <1158035520@qq.com>
2021-08-03 17:34:59 +0800

    update

--- ArchLinux.sh
+++ ArchLinux.sh
@@ -7,8 +7,8 @@ sudo pacman -S zsh
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 chsh -s /bin/zsh
 # git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-sudo pacman -S python-pip
-sudo pip install gdbgui --upgrade
+# sudo pacman -S python-pip
+# sudo pip install gdbgui --upgrade gdbgui 不知道为什么打不开
 # sudo pacman -S chromium 安装chrominum
 # sudo pacman -S firefox  安装firefox
 echo 'finish'
