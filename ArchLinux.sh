166f922d91c6bea16d255b75e2462d32da307d37
4kangjc <1158035520@qq.com>
2021-08-16 16:12:58 +0800

    update

--- ArchLinux.sh
+++ ArchLinux.sh
@@ -6,9 +6,8 @@ sudo pacman -S lsof inetutils gnu-netcat net-tools tcpdump
 sudo pacman -S openssh man man-pages neofetch
 sudo pacman -S archlinuxcn-keyring  
 sudo pacman -S yay
-sudo pacman -S zsh
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-chsh -s /bin/zsh		# 以下为桌面端环境
+sudo pacman -S zsh oh-my-zsh-git
+# 以下为桌面端环境
 # git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 # sudo pacman -S python-pip
 # sudo pip install gdbgui --upgrade gdbgui 不知道为什么打不开
