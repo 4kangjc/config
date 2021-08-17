85ba9286028951e7afba65788bd256426fdbe0cd
4kangjc <1158035520@qq.com>
2021-08-18 04:19:39 +0800

    update

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -8,7 +8,12 @@
  sudo rm gcc g++
  sudo ln -s gcc-11 gcc
  sudo ln -s g++-11 g++
+ sudo add-apt-repository ppa:longsleep/golang-backports
+ sudo apt-get update
+ sudo apt-get install golang
  sudo apt install cgdb
+ sudo apt install zsh
+ sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
  # sudo apt install python3-pip           安装gdbgui
  # sudo pip install gdbgui --upgrade
  # sudo apt install firefox               安装firefox
