c391adcf9e20209670727c783ae3160297f30096
4kangjc <1158035520@qq.com>
2021-09-04 07:45:51 +0800

    update

--- ArchLinux.sh
+++ ArchLinux.sh
@@ -11,9 +11,10 @@ sudo pacman -S zsh oh-my-zsh-git
 # git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 # sudo pacman -S python-pip
 # sudo pip install gdbgui --upgrade gdbgui 不知道为什么打不开
-# sudo pacman -S chromium 安装chrominum
-# sudo pacman -S firefox  安装firefox
-# yay -S microsoft-edge-dev-bin 
+# sudo pacman -S chromium                                                     #安装chrominum
+# sudo pacman -S firefox                                                      #安装firefox
+# sudo pacman -S google-chrome                                                #安装google-chrome
+# yay -S microsoft-edge-dev-bin                                               #安装edge
 # sudo pacman -S ntfs-3g                                                      #识别NTFS格式的硬盘
 # sudo pacman -S adobe-source-han-serif-cn-fonts wqy-zenhei                   #安装几个开源中文字体 一般装上文泉驿就能解决大多wine应用中文方块的问题
 # sudo pacman -S noto-fonts-cjk noto-fonts-emoji noto-fonts-extra             #安装谷歌开源字体及表情
@@ -22,14 +23,17 @@ sudo pacman -S zsh oh-my-zsh-git
 # sudo pacman -S gwenview                                                     #图片查看器
 # 输入f法
 # sudo pacman -S fcitx5-im
-# sudo pacman -S fcitx5-chinese-addons #官方中文输入引擎
-# sudo pacman -S fcitx5-material-color #主题
+# sudo pacman -S fcitx5-chinese-addons                                        #官方中文输入引擎
+# sudo pacman -S fcitx5-material-color                                        #主题
 # sudo pacman -S qv2ray-dev-git v2ray
-# sudo pacman -S visual-studio-code-bin goland goland-jre
-# sudo pacman -S telegram-desktop flameshot
-# yay -S listen1-desktop-appimage zy-player-bin                              #视频影音
-# sudo pacman -S vlc 							     #VLC 播放器
-# sudo pacman -S typora 						     #记笔记
-# sudo pacman -S latte-dock					  	     #dock栏
-# yay -S baidunetdisk-bin						     #百度网盘
+# sudo pacman -S visual-studio-code-bin goland goland-jre                     #编译器
+# sudo pacman -S clion clion-jre                                              # 一般有vscode就行
+# yay -S vscodium-bin                                                         #vscode bin
+# sudo pacman -S telegram-desktop flameshot                                   # 电报 截图
+# yay -S listen1-desktop-appimage zy-player-bin                               #视频影音
+# sudo pacman -S vlc 							                              #VLC 播放器
+# sudo pacman -S typora 						                              #记笔记
+# sudo pacman -S latte-dock					  	                              #dock栏
+# sudo pacman -S postman                                                      #postman
+
 echo 'finish'
