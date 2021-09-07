a015df6834a97b29705fbc22d3b56ee2d0751fb5
4kangjc <1158035520@qq.com>
2021-09-07 09:36:38 +0800

    update

--- ArchLinux.sh
+++ ArchLinux.sh
@@ -4,10 +4,10 @@ sudo pacman -S gcc boost go vim gdb cgdb
 sudo pacman -S git wget tree curl
 sudo pacman -S lsof inetutils gnu-netcat net-tools tcpdump 
 sudo pacman -S openssh man man-pages neofetch
-sudo pacman -S archlinuxcn-keyring  
-sudo pacman -S yay
-sudo pacman -S zsh oh-my-zsh-git
-cp /usr/share/oh-my-zsh/zshrc ~/.zshrc
+#sudo pacman -S archlinuxcn-keyring  
+#sudo pacman -S yay
+#sudo pacman -S zsh oh-my-zsh-git
+#cp /usr/share/oh-my-zsh/zshrc ~/.zshrc
 # 以下为桌面端环境
 # git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 # sudo pacman -S python-pip
